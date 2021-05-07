9c6117152a883bf2f93e8fce84cf39a8012a19ba
LL888 <76023384+lyli888@users.noreply.github.com>
2021-05-06 21:08:08 -0400

    f

--- 17-Mapping-Web/1/Activities/01-Ins_Basic_Map/Solved/config.js
+++
@@ -1,2 +0,0 @@
-// API key
-const API_KEY = "pk.eyJ1IjoibWNvbndhMjAiLCJhIjoiY2tucDZxM3ExMWJ1bjJ1cGV5eWtqMHJzbCJ9.AyYa7xeZUrQwA9LRQncuTg"
--- 17-Mapping-Web/3/Activities/01-Stu_CitiBike_Leaflet/Unsolved/static/js/config.js
+++
@@ -1,2 +0,0 @@
-// API key
-var API_KEY = "pk.eyJ1IjoibWNvbndhMjAiLCJhIjoiY2tucDZxM3ExMWJ1bjJ1cGV5eWtqMHJzbCJ9.AyYa7xeZUrQwA9LRQncuTg";
